507bcb56f7c1b77e6bb11af845d8d4a2fe8e233d
“Muhammad <mmuttaki@uoguelph.ca>
2021-06-26 08:42:41 -0400

    authflow done

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,   })   
+.then(() => console.log("Database connected!"))
+.catch(err => console.log(err));
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("hi");
-});
+
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
